9683d676792d3dca9299bef87995038334d873bc
Todd B. Smith <elliptic1@gmail.com>
2025-07-13 16:54:18 -0400

    Improve polynomial selection demo

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -10,7 +10,14 @@ def test_polynomial_evaluate_and_degree():
     assert poly.evaluate(2) == 17
 
 
-def test_select_polynomial():
-    poly = select_polynomial(10)
+def test_select_polynomial_degree_one():
+    poly = select_polynomial(10, degree=1)
     assert isinstance(poly, Polynomial)
     assert poly.coeffs == (-int(10 ** 0.5), 1)
+
+
+def test_select_polynomial_higher_degree():
+    poly = select_polynomial(10, degree=3)
+    assert isinstance(poly, Polynomial)
+    assert poly.degree() == 3
+    assert poly.coeffs == (-10, 0, 0, 1)
