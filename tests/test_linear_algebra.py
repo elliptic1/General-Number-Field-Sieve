d2c14b0696b38c41cdf2e58f5fd16474e8da70f4
Todd B. Smith <elliptic1@gmail.com>
2025-08-11 01:17:20 -0400

    test: expand coverage

--- tests/test_linear_algebra.py
+++ tests/test_linear_algebra.py
@@ -1,4 +1,5 @@
-from gnfs.linalg import solve_matrix
+import numpy as np
+from gnfs.linalg import solve_matrix, _nullspace_mod2
 from gnfs.sieve import Relation
 
 
@@ -10,3 +11,10 @@ def test_solve_matrix_returns_dependency():
     ]
     deps = solve_matrix(relations, primes)
     assert deps == [[0, 1]]
+
+
+def test_nullspace_mod2_basic_case():
+    matrix = np.array([[1, 1, 0], [0, 1, 1]], dtype=int)
+    basis = _nullspace_mod2(matrix)
+    assert len(basis) == 1
+    assert np.array_equal(basis[0], np.array([1, 1, 1]))
