c335523e82e7820f5e3566e928ea02caba0ea721
Todd B. Smith <elliptic1@gmail.com>
2025-09-16 12:36:12 -0400

    Add number field arithmetic for GNFS polynomials

--- gnfs/polynomial/__init__.py
+++ gnfs/polynomial/__init__.py
@@ -1,6 +1,7 @@
 """Polynomial utilities for the General Number Field Sieve."""
 
+from .number_field import NumberField, NumberFieldElement
 from .polynomial import Polynomial
 from .selection import select_polynomial
 
-__all__ = ["Polynomial", "select_polynomial"]
+__all__ = ["Polynomial", "select_polynomial", "NumberField", "NumberFieldElement"]
